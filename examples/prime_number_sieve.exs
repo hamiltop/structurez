7b57dd788760c482eef34e55fa6c2b3ab9bb3983
Peter Hamilton <phamilton@brightroll.com>
2014-08-12 22:06:47 -0700

    standardize example

--- examples/prime_number_sieve.exs
+++ examples/prime_number_sieve.exs
@@ -1,24 +1,21 @@
-{set_time, _} = :timer.tc fn ->
-  2..1000000 |> Enum.reduce TreeSet.new, fn (el, set) ->
-    has_factors = set
-      |> Stream.take_while(&(&1 <= :math.sqrt(el)))
-      |> Enum.any?( &(rem(el, &1) == 0 ) )
-    case has_factors do
-      true -> set
-      false -> Set.put(set, el)
+defmodule Primes do
+  def fill_with_primes(collection, limit) do
+    {time, _} = :timer.tc fn ->
+      2..limit |> Enum.reduce collection, fn (el, set) ->
+        has_factors = set
+          |> Stream.take_while(&(&1 <= :math.sqrt(el)))
+          |> Enum.any?( &(rem(el, &1) == 0 ) )
+        case has_factors do
+          true -> set
+          false -> Enum.into([el],set)
+        end
+      end
     end
-  end
-end
-{list_time, _} = :timer.tc fn ->
-  2..1000000 |> Enum.reduce [], fn (el, set) ->
-    has_factors = set
-      |> Stream.take_while(&(&1 <= :math.sqrt(el)))
-      |> Enum.any?( &(rem(el, &1) == 0 ) )
-    case has_factors do
-      true -> set
-      false -> set ++ [el]
-    end
+    IO.puts "#{inspect(collection)}: #{time}"
   end
 end
 
-IO.puts "TreeSet: #{set_time} vs List: #{list_time}"
+limit = 10_000_000
+
+Primes.fill_with_primes(TreeSet.new, limit)
+Primes.fill_with_primes([], limit)
